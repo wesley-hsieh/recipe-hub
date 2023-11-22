8fc3480dd3fc8d6dd66b1e77b232b57b1ed87b1d
Wesley Hsieh <whsieh1996@gmail.com>
2023-11-21 23:20:53 -0800

    Initializing basic express backend

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {NotFoundError} = require("./expressError");
+const cors = require("cors");
+
+const app = express;
+
+app.use(express.json());
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;

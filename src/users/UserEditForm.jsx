23a27f3ebb43789235ecc0380cf1ff06b7217c3c
Wesley Hsieh <whsieh1996@gmail.com>
2023-12-09 00:54:58 -0800

    added UserEditForm, allows user to edit their profile: first name, last name, email are valid things to change

---
+++ src/users/UserEditForm.jsx
@@ -0,0 +1,143 @@
+import React, { useState, useContext } from "react";
+import Alert from "../common/Alert";
+import RecipeAPI from "../api/api";
+import UserContext from "../auth/UserContext";
+
+function UserEditForm() {
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+    const [formData, setFormData] = useState({
+        firstName: currentUser.firstName,
+        lastName: currentUser.lastName,
+        email: currentUser.email,
+        username: currentUser.username,
+        password: "",
+    });
+    const [formErrors, setFormErrors] = useState([]);
+
+    // switch to use our fancy limited-time-display message hook
+    const [saveConfirmed, setSaveConfirmed] = useState(false);
+    // const [saveConfirmed, setSaveConfirmed] = useTimedMessage()
+
+    console.debug(
+        "ProfileForm",
+        "currentUser=", currentUser,
+        "formData=", formData,
+        "formErrors=", formErrors,
+        "saveConfirmed=", saveConfirmed,
+    );
+
+    /** on form submit:
+     * - attempt save to backend & report any errors
+     * - if successful
+     *   - clear previous error messages and password
+     *   - show save-confirmed message
+     *   - set current user info throughout the site
+     */
+
+    async function handleSubmit(evt) {
+        evt.preventDefault();
+
+        let profileData = {
+            firstName: formData.firstName,
+            lastName: formData.lastName,
+            email: formData.email,
+            password: formData.password,
+        };
+
+        let username = formData.username;
+        let updatedUser;
+
+        try {
+            updatedUser = await RecipeAPI.saveProfile(username, profileData);
+        } catch (errors) {
+            debugger;
+            setFormErrors(errors);
+            return;
+        }
+
+        setFormData(f => ({ ...f, password: "" }));
+        setFormErrors([]);
+        setSaveConfirmed(true);
+
+        // trigger reloading of user information throughout the site
+        setCurrentUser(updatedUser);
+    }
+
+    /** Handle form data changing */
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+        setFormData(f => ({
+            ...f,
+            [name]: value,
+        }));
+        setFormErrors([]);
+    }
+
+    return (
+        <div>
+            <h3>Profile</h3>
+            <div>
+                <div>
+                    <form>
+                        <div>
+                            <label>Username</label>
+                            <p>{formData.username}</p>
+                        </div>
+                        <div>
+                            <label>First Name</label>
+                            <input
+                                name="firstName"
+                                className="form-control"
+                                value={formData.firstName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Last Name</label>
+                            <input
+                                name="lastName"
+                                className="form-control"
+                                value={formData.lastName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <input
+                                name="email"
+                                className="form-control"
+                                value={formData.email}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Confirm password to make changes:</label>
+                            <input
+                                type="password"
+                                name="password"
+                                className="form-control"
+                                value={formData.password}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        {formErrors.length
+                            ? <Alert type="danger" messages={formErrors} />
+                            : null}
+
+                        {saveConfirmed
+                            ?
+                            <Alert type="success" messages={["Updated successfully."]} />
+                            : null}
+
+                        <button onClick={handleSubmit}>
+                            Save Changes
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserEditForm;

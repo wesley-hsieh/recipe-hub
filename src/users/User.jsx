cebfe6217630ba539ceeec5c5ccf06fb5adac5e2
Wesley Hsieh <whsieh1996@gmail.com>
2023-12-13 04:48:42 -0800

    Added styling to User.jsx and a link to front end endpoint for new recipe creation

--- src/users/User.jsx
+++ src/users/User.jsx
@@ -1,42 +1,37 @@
-import React, {useContext, useState, useEffect} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import UserContext from "../auth/UserContext";
 import RecipeAPI from "../api/api";
 import RecipeList from "../recipes/RecipeList";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./User.css"; // Import your CSS file for styling
 
-function User(){
-    const {currentUser} = useContext(UserContext);
+function User() {
+    const { currentUser } = useContext(UserContext);
     const [recipes, setRecipes] = useState([]);
 
     const fetchRecipes = async () => {
         const res = await RecipeAPI.getRecipes();
         const userRecipes = res.filter((recipe) => recipe.username === currentUser.username);
-        // console.log("userRecipes", userRecipes);
-        // for(let recipe of userRecipes){
-        //     for(let parameter in recipe){
-        //         console.log(parameter, recipe[parameter]);
-        //     }
-        // }
-
         setRecipes(userRecipes);
-    }
+    };
 
     useEffect(() => {
-        // Fetch recipes when the component mounts
         fetchRecipes();
     }, []);
 
-    return(
-        <div>
-            <h2>Welcome, {currentUser.username} !</h2>
-            <Link to="/user/edit">Edit your profile</Link>
+    return (
+        <div className="user-container">
+            <h2>Welcome, {currentUser.username}!</h2>
+            <div className="links-container">
+                <Link className="profile-link" to="/user/edit">Edit your profile</Link>
+                <Link className="add-recipe-link" to="/recipe/new">Add a recipe!</Link>
+            </div>
             <h3>Your Recipes:</h3>
             <ul>
-                <RecipeList recipes={recipes}/>
+                <RecipeList recipes={recipes} />
             </ul>
         </div>
-    )
+    );
 }
 
 export default User;
-

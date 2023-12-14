89fef9def021fb117298725e8c8cf93fe438d936
Wesley Hsieh <whsieh1996@gmail.com>
2023-12-14 02:49:51 -0800

    major feature addition to Searchform: allowing user to go to the 'next page' of recipe results. Several React state variables initialized: 1) cont/setCont is an array holding all the _cont values from API, use the values to adjust the axios request call and access different sets of recipes. 2) page/setPage is an integer representing the numerical page value. 3) query/setQuery to hold onto the user's search value. Added buttons with conditional rendering that allows the user to increment/decrement the 'page' state. useEffect with dependency on 'page' state that will appropriately submit a request to grab the next page of recipes. If the user decrements the page back to 0, use the previous 'search(query)' function to repopulate the page with the default values (did this in favor of copying excess code).

--- src/common/Searchform.jsx
+++ src/common/Searchform.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import RecipeAPI from "../api/api";
 import RecipeList from "../recipes/RecipeList";
 import "../common/SearchForm.css";
@@ -6,16 +6,23 @@ import "../common/SearchForm.css";
 function Searchform(){
     const [searchTerm, setSearchTerm] = useState("");
     const [recipes, setRecipes] = useState([]);
+    const [cont, setCont] = useState([null]);
+    const [page, setPage] = useState(null);
+    const [query, setQuery] = useState(null);
 
     async function search(query){
-        console.debug("in SearchForm");
-        let res = await RecipeAPI.queryRecipes(query);
+        console.debug("in SearchForm", query);
+        let res = await RecipeAPI.queryRecipes({name: query});
         console.log(res);
 
         const recipes = res.recipes.map(recipe => recipe.recipe);
 
         // setRecipes(res.recipes);
         setRecipes(recipes);
+
+        setPage(0);
+        setCont([null, res._cont]);
+        setQuery(query);
     }
 
     function handleSubmit(evt){
@@ -27,6 +34,37 @@ function Searchform(){
         console.debug("searchTerm handleChange");
         setSearchTerm(evt.target.value);
     }
+
+    function handlePageChange(direction){
+        if (direction === "next") {
+            setPage((page) => page + 1);
+        } else if (direction === "prev" && page > 0) {
+            setPage((page) => page - 1);
+        }
+    }
+
+    useEffect(() => {
+        console.debug("Current page: ", page);
+        async function grabPage(){
+            // console.debug(page);
+            console.debug(cont);
+            const contValue = cont[page];
+            console.debug("contVlaue: ", contValue);
+
+            let res = await RecipeAPI.queryRecipes({name: query, data: {contValue: contValue}});
+            console.debug("grabPage: ", res);
+
+            const recipes = res.recipes.map(recipe => recipe.recipe);
+            setRecipes(recipes);
+            setCont([...cont, res._cont]);
+        }
+        if(page > 0){
+            grabPage();
+        }
+        else if (page === 0){
+            search(query);
+        }
+    }, [page]);
 
     return (
         <div>
@@ -46,6 +84,12 @@ function Searchform(){
                 </form>
             </div>
             <div>
+                {recipes && recipes.length > 0 && ( // Conditionally render the button
+                    <div style={{margin: 10}}>
+                        {page > 0 && (<button className="submit-button" onClick={() => handlePageChange("prev")}>Prev Page </button>)}
+                        <button className="submit-button" onClick={() => handlePageChange("next")}>Next Page</button>
+                    </div>
+                )}
                 <RecipeList recipes={recipes}/>
             </div>
         </div>

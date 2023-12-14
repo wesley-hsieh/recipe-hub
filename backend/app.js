76beeae4b67c82428095beae3ed54f3640dc2386
Wesley Hsieh <whsieh1996@gmail.com>
2023-12-14 02:38:37 -0800

    added queryPage, Edamam API is set up so that instead of supplying an 'offset' value on request, they simply return a '_cont' value in their response object, which can then be added to the params of the url to then get the next set of values

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ const edamamRoutes = require('./routes/edamam');
 
 const app = express();
 
+/** middleware*/
 app.use(express.json());
 app.use(cors());
 app.use(authenticateJWT);
--- backend/helpers/api.js
+++ backend/helpers/api.js
@@ -18,11 +18,48 @@ async function queryAPI(query){
     try{
         const response = await axios.get(url, config);
         // console.log(response.data.hits.length);
-        return response.data.hits;
+        const nextUrl = response.data._links.next.href;
+        console.log("nexturl", nextUrl);
+        const urlObj = new URL(nextUrl);
+        console.log("urlObj", urlObj);
+        const contValue = urlObj.searchParams.get('_cont');
+        console.log("contValue:", contValue);
+        return {queryRecipes: response.data.hits, _cont: contValue};
     }catch(error){
         throw new BadRequestError();
     }
 
 }
 
-module.exports = queryAPI;
+async function queryPage(query, cont){
+    console.log("in queryPage");
+    const url = `https://api.edamam.com/api/recipes/v2?type=any&q=${query}&_cont=${cont}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+    const config = {
+        headers: {
+            'Accept': 'application/json',
+            'Accept-Language': 'en'
+        }
+    };
+
+    try {
+        const response = await axios.get(url, config);
+        const nextUrl = response.data._links.next.href;
+        console.log("nexturl", nextUrl);
+        const urlObj = new URL(nextUrl);
+        console.log("urlObj", urlObj);
+        const newContValue = urlObj.searchParams.get('_cont');
+        console.log("newContValue:", newContValue);
+        return { queryRecipes: response.data.hits, _cont: newContValue };
+    } catch (error) {
+        throw new BadRequestError();
+    }
+
+}
+
+
+
+module.exports = {
+    queryAPI,
+    queryPage
+};

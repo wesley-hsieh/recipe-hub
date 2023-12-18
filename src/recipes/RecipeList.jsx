ffbdf0f6e8ddf494259ffaa5cae7bca41367a676
Wesley Hsieh <whsieh1996@gmail.com>
2023-12-18 04:43:28 -0800

    adjusted RecipeList.jsx to include the recipe instructions with conditionals to take into account whether or not the recipes are from the database or the API

--- src/recipes/RecipeList.jsx
+++ src/recipes/RecipeList.jsx
@@ -12,21 +12,39 @@ import "./RecipeList.css";
  */
 
 function RecipeList({ recipes }) {
-    return (
-        <div className="recipe-list">
-            {recipes.map((recipe, index) => (
-                <div key={index} className="recipe-card-container">
-                    <RecipeCard
-                        id={recipe.id}
-                        image={recipe.image ? recipe.image : null}
-                        label={recipe.label}
-                        ingredients={recipe.ingredientLines ? recipe.ingredientLines : recipe.ingredients.split(",")}
-                        url={recipe.url ? recipe.url : null}
-                    />
-                </div>
-            ))}
-        </div>
-    );
+
+    const getInstructions = (recipe) => {
+        if (recipe.instructionLines) {
+            return recipe.instructionLines;
+        } else if (recipe.instructions) {
+            return recipe.instructions.split(",");
+        }
+        return null;
+    };
+
+    const recipeCards = recipes.map((recipe, index) => {
+        const instructions = getInstructions(recipe);
+
+        return (
+            <div key={index} className="recipe-card-container">
+                <RecipeCard
+                    id={recipe.id}
+                    image={recipe.image ? recipe.image : null}
+                    label={recipe.label}
+                    api_uri={recipe.uri}
+                    ingredients={
+                        recipe.ingredientLines
+                            ? recipe.ingredientLines
+                            : recipe.ingredients.split(",")
+                    }
+                    instructions={instructions}
+                    url={recipe.url ? recipe.url : null}
+                />
+            </div>
+        );
+    });
+
+    return <div className="recipe-list">{recipeCards}</div>;
 }
 
 export default RecipeList;
